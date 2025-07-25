235d59a2d2290368d417bac168b58594eb28f122
Kataleko <katalekoweb@gmail.com>
2025-07-25 23:31:11 +0100

    fix routes

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/', views.BlogPostListCreate.as_view(), name='blogpost-view-create'),
+    path('', views.BlogPostListCreate.as_view(), name='blogpost-view-create'),
     path('<int:pk>/', views.BlogPostRetrieveUpdateDestroy.as_view(), name='update-blogpost'),
 ]
